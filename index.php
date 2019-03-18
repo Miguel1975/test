2f5dbacda6bbdf2227ffa2ddf994851a464375d2
Miguel Webmaster <m.faria@elsevier.com>
2019-03-18 15:50:23 +0100

    files added

---
+++ index.php
@@ -0,0 +1,136 @@
+<?php
+$incHeader= $_SERVER['DOCUMENT_ROOT'];
+include ($incHeader."/Includes-FR/header.php");
+?>
+<link href="https://fonts.googleapis.com/css?family=EB+Garamond" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="https://eu-ireland-custom-media-prod.s3-eu-west-1.amazonaws.com/Other/ElsevierBrandGuidelines-Assets/css/els-brd-guide-v1.css">
+<div class="row bg-grays" style="margin-left: 0rem; margin-right: 0rem;">
+<div class="large-12 medium-12 small-12 columns">
+<div class="row">
+<div class="large-12 medium-12 small-12 columns">
+<h1 class="header-grays text-center hide-for-small-only" style="font-size: 1.5em; margin-bottom: 2%;">D&eacute;couvrez cet atlas indispensable sur l&rsquo;anatomie en corr&eacute;lation avec la pratique clinique</h1>
+<h4 class="header-grays-small text-center show-for-small-only">D&eacute;couvrez cet atlas indispensable sur l&rsquo;anatomie en corr&eacute;lation avec la pratique clinique</h4>
+<!--	<hr>--></div>
+</div>
+<!-- Prix, Titre Livre-->
+<div class="row">
+<div class="large-6 medium-6 small-12 columns text-center" style="background-image: nomForm(;">
+<h2 class="book-title hide-for-small-only" style="text-align: center; color: #53565a; font-size: 2.5em; margin-top: 7%;">GRAY&rsquo;S</h2>
+<h3 class="subtitle hide-for-small-only" style="text-align: center; color: #53565a; font-size: 1.6em;">Atlas d&rsquo;anatomie humaine</h3>
+<p class="author hide-for-small-only" style="text-align: center; color: #53565a; font-size: 1em;">Auteurs : Richard L. Drake, A. Wayne Vogl, Adam W. M. Mitchell, Richard <br /> M. Tibbitts, Paul E. Richardson</p>
+<!-- bouton desktop + couverture-->
+<script type="text/javascript">
+function ajouterAuPanier(nomForm){
+
+
+var valeur = nomForm.elements['productType'];
+
+		for (i=0; i<valeur.length; i++) {
+			if (valeur[i].checked) {
+
+				window.open(valeur[i].value);
+				break;
+
+
+			}
+		}
+	}
+
+</script>
+<div class="row">
+<div class="large-6 medium-6 small-12 columns medium-centered hide-for-small-only" style="padding-top: 20px;">
+<h3 class="text-center" style="margin-bottom: 20px;">Choisissez votre format</h3>
+<form name="prixItem"   style="text-align: left;">
+	<label class="els-container" style="margin-left: 20%;">Livre 79,00 &euro; 
+		<input checked="checked" name="productType" type="radio" value="https://www.elsevier-masson.fr/index.php/checkout/cart/add?product=36168&amp;qty=1" /> 
+		<span class="els-radio-checkmark"></span> 
+	</label> 
+	<label class="els-container" style="margin-left: 20%;">E-book 49,98 &euro; 
+		<input name="productType" type="radio" value="https://www.elsevier-masson.fr/index.php/checkout/cart/add?product=83115&amp;qty=1" />
+		<span class="els-radio-checkmark"></span>
+	</label>
+<p class="text-center" style="padding-top: 20px;"><input class="els-primary-md-button"  type="submit" value="Ajouter au panier" onclick="ajouterAuPanier(this.form);"/></p>
+</form></div>
+</div>
+</div>
+<div class="large-6 medium-6 small-12 columns">
+<h2 class="book-title-small show-for-small-only" style="text-align: center; color: #53565a; font-size: 2em; margin-top: 4%;">GRAY&rsquo;S</h2>
+<h3 class="subtitle-small show-for-small-only" style="text-align: center; color: #53565a; font-size: 1.7em; margin-top: 3%;">Atlas d&rsquo;anatomie humaine</h3>
+<p class="author-small show-for-small-only" style="text-align: center; color: #53565a; font-size: 1em;">Auteurs : Richard L. Drake, A. Wayne Vogl, Adam W. M. Mitchell, Richard M. Tibbitts, Paul E. Richardson</p>
+<img alt="" src="https://eu-ireland-custom-media-prod.s3-eu-west-1.amazonaws.com/France/LandingPage/LP-Grays/big-grays-book.png" /></div>
+<!-- Bouton +couverture Mobile-->
+<div class="row">
+<div class="small-12 columns medium-centered show-for-small-only" style="padding-top: 20px;">
+<h3 class="text-center" style="margin-bottom: 20px;">Choisissez votre format</h3>
+
+
+
+<form action="#"  name="mobilePrixItem" style="text-align: left; margin-bottom: 20px">
+	<label class="els-container" style="margin-left: 20%;">Livre 79,00 &euro; 
+		<input checked="checked" name="productType" type="radio" value="https://www.elsevier-masson.fr/index.php/checkout/cart/add?product=36168&amp;qty=1" />
+		<span class="els-radio-checkmark"></span>
+	</label> 
+	<label class="els-container" style="margin-left: 20%;">E-book 49,98 &euro; 
+		<input name="productType" type="radio" value="https://www.elsevier-masson.fr/index.php/checkout/cart/add?product=83115&amp;qty=1" /> 
+		<span class="els-radio-checkmark"></span> 
+	</label>
+<p class="text-center" style="padding-top: 20px;"><input class="els-primary-md-button"  type="submit" value="Ajouter au panier" onclick="ajouterAuPanier(this.form);" /></p>
+</form>
+
+</div>
+</div>
+</div>
+<!-- Ebook Bullet Point -->
+<div class="row" style="background-color: #fff0e4;">
+<div class="large-6 medium-6 small-12 columns">
+<h2 class="show-for-small-only title-bulletpoint-small text-center" style="color: #53565a; margin-top: 18%;">L&rsquo;atlas clair et didactique en anatomie</h2>
+<img src="https://eu-ireland-custom-media-prod.s3-eu-west-1.amazonaws.com/France/LandingPage/LP-Grays/iphone-hand.png" /></div>
+<div class="large-6 medium-6 small-12 columns bulletpoint" style="color: #53565a; margin-top: 18%;">
+<h2 class="hide-for-small-only" style="font-size: 2.5em;">L&rsquo;atlas clair et didactique en anatomie</h2>
+<ul class="hide-for-small-only els-list els-ul">
+<li>Illustrations claires</li>
+<li>Scanners, &eacute;chographies, radiographies en regard des planches</li>
+<li>Nombreux tableaux de synth&egrave;se</li>
+<li>E-book offert avec le livre papier</li>
+</ul>
+<ul class="small-bullet show-for-small-only els-list els-ul" style="margin-bottom: 15%;">
+<li>Illustrations claires</li>
+<li>Scanners, &eacute;chographies, radiographies en regard des planches</li>
+<li>Nombreux tableaux de synth&egrave;se</li>
+<li>E-book offert avec le livre papier</li>
+</ul>
+</div>
+</div>
+<!-- Témoignage -->
+<div class="row feedback hide-for-small-only">
+<div class="large-12 medium-12 small-12 columns" style="margin-top: 40px;">
+<p style="font-style: italic; color: #53565a; font-size: 1.3rem;">&ldquo;La <strong>simplicit&eacute; de son utilisation</strong> qui permet une recherche rapide, <strong>Le grand format des planches</strong> et ses <strong>dessins de qualit&eacute;s</strong> permettent une <strong>approche claire des stuctures anatomiques</strong>, sont pour moi un vrai point positif de cet ouvrage&rdquo;</p>
+<p style="font-size: 1rem; color: #53565a;">Charlotte, 5<sup>&egrave;me</sup> ann&eacute;e de m&eacute;decine, Bordeaux</p>
+</div>
+</div>
+<div class="row feedback-small show-for-small-only">
+<div class="large-12 medium-12 small-12 columns">
+<p style="font-style: italic; color: #53565a; font-size: 1.1rem; margin-top: 10%;">&ldquo;La <strong>simplicit&eacute; de son utilisation</strong> qui permet une recherche rapide, <strong>Le grand format des planches</strong> et ses <strong>dessins de qualit&eacute;s</strong> permettent une <strong>approche claire des stuctures anatomiques</strong>, sont pour moi un vrai point positif de cet ouvrage&rdquo;</p>
+<p style="margin-top: 10px; color: #53565a; margin-bottom: 8%;">Charlotte, 5&egrave;me ann&eacute;e de m&eacute;decine, Bordeaux</p>
+</div>
+</div>
+<!-- Download -->
+<div class="row">
+<div class="large-6 medium-6 small-12 columns"><img alt="" src="https://eu-ireland-custom-media-prod.s3-eu-west-1.amazonaws.com/France/LandingPage/LP-Grays/download-img.png" /></div>
+<div class="large-6 medium-6 small-12 columns download-text hide-for-small-only">
+<p class="text-center" style="margin-top: 18%; color: #53565a; font-size: 1.5rem;">Pour vous, <strong>d&eacute;couvrez le chapitre</strong> sur le corps <strong>en acc&egrave;s libre</strong></p>
+<p class="text-center"><em class="text-center download-format" style="font-size: 1.5rem; color: #53565a;">Format pdf - 2,9 Mo</em></p>
+<a class="els-tertiary-download" href="https://www.elsevier-masson.fr/media/wysiwyg/France/Download/Drake474780/Drake474780.pdf" style="margin-top: 12%; margin-left: 30%;" target="_blank">T&eacute;l&eacute;charger le chapitre</a></div>
+<div class="large-6 medium-6 small-12 columns download-text-small show-for-small-only">
+<p class="text-center" style="margin-top: 4%; color: #53565a; font-size: 1.2rem;">Pour vous, <strong>d&eacute;couvrez le chapitre</strong> sur le corps <strong>en acc&egrave;s libre</strong></p>
+<p class="text-center"><em class="text-center download-format" style="color: #53565a; font-size: 1rem;">Format pdf - 2,9 Mo</em></p>
+<a class="els-tertiary-md-download" href="https://www.elsevier-masson.fr/media/wysiwyg/France/Download/Drake474780/Drake474780.pdf" target="_blank" style="margin-top: 5%; margin-left: 15%; margin-bottom: 10%;">T&eacute;l&eacute;charger le chapitre</a>
+</div>
+</div>
+</div>
+</div>
+
+<?php
+$incHeader= $_SERVER['DOCUMENT_ROOT'];
+include ($incHeader."/Includes-FR/footer.php");
+?>
